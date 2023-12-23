5d8a1bab1c91e07a2478544c9f290e79d4676d9d
Junaid Sadiq <69793438+Junaid-sadiq@users.noreply.github.com>
2023-12-23 15:58:27 +0200

    🙏

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -1,23 +1,32 @@
+
 def debug_print(debug_msg=None, **kwargs):
+
     if debug_msg:
         print(debug_msg)
+
     for key, value in kwargs.items():
         print("{}: {}".format(key, value))
 
-def mergesort(array, depth=0):
-    indent = "  " * depth  # For better readability of debug output
+
+def mergesort(array):
+    debug_print(array=array)
     if len(array) <= 1:
         return array
 
     m = len(array) // 2
+    debug_print(m=m)
 
     left = mergesort(array[:m])
     right = mergesort(array[m:])
 
     return merge(left, right)
 
+
 def merge(left, right):
+    debug_print(debug_msg="Merging...", left=left, right=right)
+
     merged = []
+
     while len(left) > 0 and len(right) > 0:
         if left[0] <= right[0]:
             merged.append(left.pop(0))
@@ -25,15 +34,19 @@ def merge(left, right):
             merged.append(right.pop(0))
 
     if len(left) > 0:
-        merged.extend(left)
-    else: 
-        merged  += right
+        merged += left
+    else:
+        merged += right
 
+    debug_print(merged=merged)
     return merged
+
 
 if __name__ == "__main__":
     input_str = input("Enter numbers, separated by ',': ")
+
     input_list = input_str.split(",")
+    debug_print(input_list=input_list)
 
     value_list = []
     for x in input_list:
@@ -43,5 +56,7 @@ if __name__ == "__main__":
             print("Invalid input.")
             quit(1)
 
+    debug_print(value_list=value_list)
+
     sorted_list = mergesort(value_list)
-    print(sorted_list)  # Print final sorted list
+    print(sorted_list)
